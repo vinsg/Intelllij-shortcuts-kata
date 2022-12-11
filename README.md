c7a64f82832c473c6e6517c9c743fcd04f6f5483
Vincent S.-G <hello@vinsg.ca>
2022-12-11 15:27:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shortcuts
+
+A simple project used to learn IntelliJ IDEA shortcuts.
+
+## Usage
+Go through the files every day to build memory and muscle memory.
+Fold all the answers to hide the solutions.
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+
+
+    // Search Everywhere
+    /*
+     Answer:
+     Double ⇧
+     Quickly find any file, action, symbol, tool window, or setting in IntelliJ IDEA, in your project, and in the current Git repository.
+     */
+
+    // Fix this
+    /*
+    Answer:
+    Alt + enter Suggestions for error correction
+     */
+    fun error(): HashMap<Int, String> {
+        val strings: HashMap<Int, String> = HashMap<Int, String>
+        return strings
+    }
+    error()
+
+
+}
